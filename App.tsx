4a5a7224cafe23958e39d77e420abe0487cb4708
Rafiq <Rafiq@itconquest.com>
2023-08-05 09:05:38 +0600

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {createStackNavigator} from "@react-navigation/stack";
+
+const AuthStack = createStackNavigator();
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Open up App.tsx to start working on your app! Some more text here ......</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/providers/AuthProvider.tsx
@@ -0,0 +1,25 @@
+import React, {createContext, useState} from "react";
+
+type IAuthProvider = {
+    userName: string,
+    setUserName: (userName: string) => void,
+    isLoggedIn: boolean,
+    setIsLoggedIn: (isLoggedIn: boolean) => void,
+    email:string,
+    setEmail: (email: string) => void
+}
+
+const AuthContext = createContext<IAuthProvider | null>(null);
+
+const AuthProvider = (props: any) => {
+    const [userName, setUserName] = useState('');
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    return (
+        <AuthContext.Provider value={{userName, setUserName, isLoggedIn, setIsLoggedIn, email, setEmail}}>
+            {props.children}
+        </AuthContext.Provider>
+    )
+}
+
+export {AuthProvider, AuthContext, IAuthProvider};
